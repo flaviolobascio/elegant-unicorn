e0a86c94cd15b7f1dc63caa1e91cabe337e3dc0a
[dyad] <flaviolobascio@gmail.com>
2025-05-10 22:51:52 +0200

    [dyad] Rimosso componente HelloWorld - wrote 1 file(s), deleted 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,15 @@
-import HelloWorld from "@/components/HelloWorld";
+import React from 'react';
 
 const Index = () => {
   return (
-    <HelloWorld />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Pagina Principale
+      </h1>
+      <p className="mt-2 text-lg text-gray-600">
+        Il componente HelloWorld è stato rimosso.
+      </p>
+    </div>
   );
 };
 
