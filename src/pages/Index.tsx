6da66c870965f50bdd855a2d03d7902de2919bd7
[dyad] <flaviolobascio@gmail.com>
2025-05-10 15:19:26 +0200

    [dyad] Creata app "Hello World" animata - wrote 2 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,8 @@
-import TurnSimulator from "@/components/TurnSimulator";
+import HelloWorld from "@/components/HelloWorld";
 
 const Index = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-slate-900 to-slate-700 p-4">
-      <TurnSimulator />
-    </div>
+    <HelloWorld />
   );
 };
 
