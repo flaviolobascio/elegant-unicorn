0d260c539f15cdf5f7c152995ba339366d7f9f30
[dyad] <flaviolobascio@gmail.com>
2025-05-11 02:42:43 +0200

    [dyad] Ripristinato TurnSimulator - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,10 @@
+import TurnSimulator from "@/components/TurnSimulator";
 import React from 'react';
 
 const Index = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <h1 className="text-4xl font-bold text-gray-800">
-        Pagina Principale
-      </h1>
-      <p className="mt-2 text-lg text-gray-600">
-        Il componente HelloWorld è stato rimosso.
-      </p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 p-4">
+      <TurnSimulator />
     </div>
   );
 };
